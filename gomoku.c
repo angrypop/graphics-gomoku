2f3a3192e1e11cfe977fcd898e85a73f73167ca5
angrypop <waangrypop@gmail.com>
2019-05-09 10:48:31 +0800

    Underlying structure to simulate Gomoku
    struct Board
    function CheckWin()
    function InitBoard()
    function SetPiece()

--- gomoku.c
+++ gomoku.c
@@ -2,3 +2,53 @@
 	Provide a gomoku board, and you can play as in real world.
 	Function definitions go here.
 */
+
+#ifndef PRAGMAONCE #define PRAGMAONCE
+#include "gomoku.h"
+
+char CheckWin(Board B) {
+	int x = 1, y = 1, dx = 0, dy = 0;
+	for (x = 1; x <= BOARDSIZE; x++) {
+		for (y = 1; y <= BOARDSIZE; y++) {
+			for (dx = -1; dx <= 1; dx++) {
+				for (dy = -1; dy <= 1; dy++) {
+					if (dx == 0 && dy == 0) continue;
+					if (x + 4 * dx > BOARDSIZE || x + 4 * dx < 1 || y + 4 * dy > BOARDSIZE || y + 4 * dy < 1) continue;
+					if (B.BoardStatus[x + 0 * dx][y + 0 * dy] == 'B' &&
+						B.BoardStatus[x + 1 * dx][y + 1 * dy] == 'B' &&
+						B.BoardStatus[x + 2 * dx][y + 2 * dy] == 'B' &&
+						B.BoardStatus[x + 3 * dx][y + 3 * dy] == 'B' &&
+						B.BoardStatus[x + 4 * dx][y + 4 * dy] == 'B'
+						)
+						return 'B';
+					if (B.BoardStatus[x + 0 * dx][y + 0 * dy] == 'W' &&
+						B.BoardStatus[x + 1 * dx][y + 1 * dy] == 'W' &&
+						B.BoardStatus[x + 2 * dx][y + 2 * dy] == 'W' &&
+						B.BoardStatus[x + 3 * dx][y + 3 * dy] == 'W' &&
+						B.BoardStatus[x + 4 * dx][y + 4 * dy] == 'W'
+						)
+						return 'W';
+				}
+			}
+		}
+	}
+	return 'N';
+}
+
+void InitBoard(Board *B) {
+	int x = 1, y = 1;
+	for (x = 1; x <= BOARDSIZE; x++) {
+		for (y = 1; y <= BOARDSIZE; y++) {
+			B->BoardStatus[x][y] = 'N';
+		}
+	}
+	B->Turn = 0;
+}
+
+void SetPiece(Board *B, int TagX, int TagY, char Side) {
+	if (B->BoardStatus[TagX][TagY] != 'N') printf("Invalid Move.\n");
+	B->BoardStatus[TagX][TagY] = Side;
+	B->Turn++;
+}
+
+#endif
--- gomoku.h
+++ gomoku.h
@@ -2,3 +2,45 @@
 	Provide a gomoku board, and you can play as in real world.
 	Function prototypes and data structures go here.
 */
+
+#ifndef PRAGMAONCE #define PRAGMAONCE
+
+#define BOARDSIZE 15
+
+struct Board {
+	unsigned int Turn;
+	char BoardStatus[BOARDSIZE + 1][BOARDSIZE + 1];
+	/*
+		BoardStatue[x][y]: 'W' = White, 'B' = Black, 'N' = Neutral
+		x and y is counted from bottom-left, ranging from 1 to 15
+	*/
+};
+typedef struct Board Board;
+
+/*
+	If a function uses pointer to a board as its parameter, it will modify the board. 
+	Otherwise it will visit only.
+*/
+
+char CheckWin(Board B);
+/* 
+	Check if any player wins.
+	Arguments	: A board
+	Return		: 'B' or 'W' or 'N'
+*/
+
+void InitBoard(Board *B);
+/*
+	Initialize a board, setting Turn as 0 and all positions as neutral.
+	Arguments	: Address of a board
+	Return		: void
+*/
+
+void SetPiece(Board *B, int TagX, int TagY, char Side);
+/* 
+	Set a piece.
+	Arguments	: Address of a board, target coordinates, color of piece
+	Return		: void
+*/
+
+#endif
