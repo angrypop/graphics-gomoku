bb26cf8c99847850708c8f6c9d159c91686d7861
Steven <3180101968@zju.edu.cn>
2019-05-11 21:10:11 +0800

    fix bug

--- extrafunc.h
+++ extrafunc.h
@@ -1,5 +1,21 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <wincon.h>
+#include <Windows.h>
+
+#include "genlib.h"
+#include "gcalloc.h"
+#include "strlib.h"
+#include "extgraph.h"
+
 //This header contains the declarations of the function added to the graphics.c
 
 //Added Function by Steven Chen
