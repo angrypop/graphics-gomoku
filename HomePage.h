c284d251026bf4a7c29e03fcd2e788afafbc8040
shenxiaowan <50399231+shenxiaowan@users.noreply.github.com>
2019-05-14 22:29:13 +0800

    Add files via upload

--- HomePage.h
+++ HomePage.h
@@ -2,6 +2,30 @@
     provide a homepage,just a start page
 */
 
+#ifdef static double winwidth, winheight; 
+#define static int    show_more_buttons = 0; 
+
+void DisplayClear(void);
+/*
+    provided in libgraphics.
+*/
+
+void startTimer(int id, int timeinterval);
+/*
+    provided in libgraphics.
+*/
+
+void display(void);
+/*
+    functions that the user displays.
+	Return    :void
+*/
+void MouseEventProcess(int x, int y, int button, int event);
+/*
+    the user's mouse event response function.
+	Return   :void
+*/
+
 void DrawButtons();
 /*
     draw four buttons on the start page.
@@ -9,4 +33,4 @@ void DrawButtons();
     Return   :void
 */
 
-
+#endif
