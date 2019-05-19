8dfaf24c9e6142175e1b2b1722407a68c3a8be24
shenxiaowan <50399231+shenxiaowan@users.noreply.github.com>
2019-05-19 10:25:49 +0800

    Add files via upload

--- HomePage.h
+++ HomePage.h
@@ -2,68 +2,27 @@
     provide a homepage,just a start page
 */
 
-#ifndef static double winwidth, winheight;
-#define static double winwidth, winheight; 
-#define static int    show_more_buttons = 0; 
-#define static int    show_model = 0;
-#define static int    show_forerunner = 0;
-#define static int    show_color = 0;
+#include "graphics.h"
+#include "extgraph.h"
+#include "genlib.h"
+#include "simpio.h"
+#include "conio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
-static void DisplayClear(void);
-/*
-    provided in libgraphics.
-*/
+#include <windows.h>
+#include <olectl.h>
+#include <mmsystem.h>
+#include <wingdi.h>
+#include <ole2.h>
+#include <ocidl.h>
+#include <winuser.h>
 
-static void startTimer(int id, int timeinterval);
-/*
-    provided in libgraphics.
-*/
+#include "imgui.h"
+#include "extrafunc.h"
 
-static void display1(void);
-/*
-    functions that the user displays.
-	Return    :void
-*/
-
-static void display2(void);
-/*
-	functions that the user displays.
-	Return    :void
-*/
-
-static void display3(void);
-/*
-	functions that the user displays.
-	Return    :void
-*/
-
-static void MouseEventProcess(int x, int y, int button, int event);
-/*
-    the user's mouse event response function.
-	Return   :void
-*/
-
-static void DrawButtons1();
-/*
-    draw four buttons on the start page.
-    Arguments:Four buttons
-    Return   :void
-*/
-
-static void DrawButtons2();
-/*
-	draw three buttons on the left to show some settings.
-	Arguments:six buttons
-	Return   :void
-*/
-
-static void DrawButtons3();
-/*
-	show some instructions.
-	Return   :void
-*/
-
-static bool HomePage(void);
+bool HomePage(void);
 /*
     show the homepage.
 	Return   :bool
