7f7f1e5dca5fb37e9afa77f94bc36dc888f41d57
Steven <3180101968@zju.edu.cn>
2019-05-22 15:15:33 +0800

    fix bug

---
+++ EndGamePage.h
@@ -0,0 +1,36 @@
+/*
+    provide a endgamepage,just a end page
+*/ 
+#ifndef _END_GAME_PAGE_H_
+#define _END_GAME_PAGE_H_
+
+#include "graphics.h"
+#include "extgraph.h"
+#include "genlib.h"
+#include "simpio.h"
+#include "conio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <windows.h>
+#include <olectl.h>
+#include <mmsystem.h>
+#include <wingdi.h>
+#include <ole2.h>
+#include <ocidl.h>
+#include <winuser.h>
+
+#include "imgui.h"
+#include "Main.h"
+
+
+
+
+
+//function
+void DisplayClear(void); 
+void MouseEventProcess(int x, int y, int button, int event);
+void display(void); 
+
+
+#endif 
